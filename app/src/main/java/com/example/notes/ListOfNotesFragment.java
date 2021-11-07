37fd91886c659ea4fd8161b0419a524952a60e77
Lettka <flower.violetta@gmail.com>
2021-11-08 00:15:44 +0200

    Initial commit

---
+++ app/src/main/java/com/example/notes/ListOfNotesFragment.java
@@ -0,0 +1,25 @@
+package com.example.notes;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListOfNotesFragment extends Fragment {
+
+    public ListOfNotesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_of_notes, container, false);
+    }
+
+
+}
